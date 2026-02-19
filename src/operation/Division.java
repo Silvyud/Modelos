7d68530d9c1147748cb1ea30693f3d41e1df4d2f
Silvyud <silvi.mp1217@gmail.com>
2026-02-18 19:09:30 -0500

    Modify ineficient attributes use

--- src/operation/Division.java
+++ src/operation/Division.java
@@ -2,13 +2,8 @@ package operation;
 
 public class Division extends Operation {
 
-    float numberA;
-    float numberB;
-
     public float operate(float numberA, float numberB) {
-        this.numberA = numberA;
-        this.numberB = numberB;
-        return this.numberA / this.numberB;
+        return numberA / numberB;
     }
 
 }
--- src/operation/Multiplication.java
+++ src/operation/Multiplication.java
@@ -5,8 +5,6 @@ import factoryMethod.OperationFactoryMethod;
 
 public class Multiplication extends Operation {
 
-    float numberA;
-    float numberB;
     OperationFactoryMethod additionFactoryMethod = new AdditionFactoryMethod();
     Operation addition;
 
@@ -15,12 +13,11 @@ public class Multiplication extends Operation {
     }
 
     public float operate(float numberA, float numberB) {
-        this.numberA = numberA;
-        this.numberB = numberB;
 
-        float result;
-        for(result = 0.0F; this.numberB > 0.0F; --this.numberB) {
-            result = this.addition.operate(result, this.numberA);
+        float result = 0;
+        while (numberA > 0) {
+            result = this.addition.operate(result, numberB);
+            numberA--;
         }
 
         return result;
